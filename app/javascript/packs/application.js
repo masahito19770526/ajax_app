920654306f9fc1d6f3aad3b66c057d28a879b45d
masahito@19770526 <masahito.shimada@enishi-advisory.com>
2023-05-13 21:36:22 +0900

    update

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../memo")  // 追記する
-require("../post")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
